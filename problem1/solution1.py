418dc08a7d60d03070df252ab86a7031ff5b3057
Hampus Serneke <hampus.serneke@gmail.com>
2024-12-06 16:34:11 +0100

    P1: fix docstring

--- problem1/solution1.py
+++ problem1/solution1.py
@@ -1,11 +1,24 @@
 from typing import List, Tuple
 import fileinput
 
-def read_data(seperator: str ="   ") -> Tuple[List[int], List[int]]:
+# Run program with the command "python solution1.py < input.txt"
+
+def read_data(separator: str ="   ") -> Tuple[List[int], List[int]]:
+    '''
+    Reads input from a file with two columns of integers, separated with a specified string.
+
+    Args:
+        separator (str): The string that separates the columns (defaults to three spaces).
+    
+    Returns:
+        Tuple[List[int], List[int]]: A tuple with the two corredsponding lists.
+
+    '''
+
     left, right = [], []
 
     for line in fileinput.input():
-        splitted = line.split(seperator)
+        splitted = line.split(separator)
         left.append(int(splitted[0]))
         right.append(int(splitted[1]))
 
@@ -14,6 +27,8 @@ def read_data(seperator: str ="   ") -> Tuple[List[int], List[int]]:
 
 def main() -> None:
     left, right = read_data()
+
+
 
 if __name__ == "__main__":
     main()
